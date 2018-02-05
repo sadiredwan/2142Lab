6f3b6c7fd8af3d7822a7d57f42bf6afbc0c6b692
JOYRU <joycseru@gmail.com>
2018-02-05 23:01:07 +0600

    add Button

--- app/src/main/java/com/nano/projectjonobak/Home.java
+++ app/src/main/java/com/nano/projectjonobak/Home.java
@@ -8,7 +8,10 @@ package com.nano.projectjonobak;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class Home  extends AppCompatActivity implements View.OnClickListener
 {
+    private Button categoriesButton, helpButton, accountSettingButton;
+    
 }
