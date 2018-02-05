cc0def3eadc850da5fe8786801c2fc3cabdee7bf
JOYRU <joycseru@gmail.com>
2018-02-06 02:29:39 +0600

    add a class CategoriesActivity and extends AppCompatactivity

---
+++ app/src/main/java/com/nano/projectjonobak/CategoriesActivity.java
@@ -0,0 +1,9 @@
+package com.nano.projectjonobak;
+
+/**
+ * Created by User on 06-Feb-18.
+ */
+
+public class CategoriesActivity extends AppCompatActivity implements View.OnClickListener {
+    
+}
