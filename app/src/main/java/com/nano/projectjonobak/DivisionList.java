f2ec01c3206bb00ae3b2a4ed1cd21123264a45d7
JOYRU <joycseru@gmail.com>
2018-02-07 02:40:26 +0600

    sucessful add Rangpur District class

--- app/src/main/java/com/nano/projectjonobak/DivisionList.java
+++ app/src/main/java/com/nano/projectjonobak/DivisionList.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.nano.projectjonobak.ChittagongDistrict;
+import com.nano.projectjonobak.divisions.ChittagongDistrict;
 import com.nano.projectjonobak.divisions.RajshahiDistrict;
 
 
@@ -56,11 +56,11 @@ public class DivisionList extends AppCompatActivity implements View.OnClickListe
         }
 
 
-     /*   else if (v.getId () == R.id.RangpurDivisionId) {
+        else if (v.getId () == R.id.RangpurDivisionId) {
 
             Intent intent = new Intent ( DivisionList.this,RangpurDistrict.class );
             startActivity ( intent );
-        }*/
+        }
 
 
 
--- app/src/main/java/com/nano/projectjonobak/RangpurDistrict.java
+++ app/src/main/java/com/nano/projectjonobak/RangpurDistrict.java
@@ -3,11 +3,11 @@ package com.nano.projectjonobak;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ChittagongDistrict extends AppCompatActivity {
+public class RangpurDistrict extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chittagong_district);
+        setContentView(R.layout.activity_rangpur_district);
     }
 }
