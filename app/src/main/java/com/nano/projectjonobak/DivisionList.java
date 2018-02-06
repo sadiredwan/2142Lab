b325d78fb792be6bada0ba14c73dec984233071f
sadiredwan <sadi.redwan@gmail.com>
2018-02-06 23:11:47 +0600

    ChittagongDistrict.java & corresponding layouts added.

--- app/src/main/java/com/nano/projectjonobak/DivisionList.java
+++ app/src/main/java/com/nano/projectjonobak/DivisionList.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.nano.projectjonobak.divisions.ChittagongDistrict;
 import com.nano.projectjonobak.divisions.RajshahiDistrict;
 
 public class DivisionList extends AppCompatActivity implements View.OnClickListener{
@@ -50,7 +51,7 @@ public class DivisionList extends AppCompatActivity implements View.OnClickListe
         else if (v.getId () == R.id.ChittagongDivisionId)
         {
 
-            Intent intent = new Intent ( DivisionList.this,RajshahiDistrict.class );
+            Intent intent = new Intent ( DivisionList.this,ChittagongDistrict.class );
             startActivity ( intent );
         }
 
