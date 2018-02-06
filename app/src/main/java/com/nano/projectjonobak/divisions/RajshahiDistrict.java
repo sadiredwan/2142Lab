c559047f87732a515c5b9a4c9243b904fd509c4a
nahid597 <nahidh597@gmail.com>
2018-02-07 03:08:24 +0600

    This is Rajshahi Divisions districts activity and class.

--- app/src/main/java/com/nano/projectjonobak/divisions/RajshahiDistrict.java
+++ app/src/main/java/com/nano/projectjonobak/divisions/RajshahiDistrict.java
@@ -1,15 +1,61 @@
 package com.nano.projectjonobak.divisions;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.nano.projectjonobak.BograRajshahi;
+import com.nano.projectjonobak.DivisionList;
 import com.nano.projectjonobak.R;
 
-public class RajshahiDistrict extends AppCompatActivity {
+public class RajshahiDistrict extends AppCompatActivity implements View.OnClickListener {
 
+    private Button BograButton, ChapaiButton, JoypurhatButton, NaogaonButton, NatoreButton, PabnaButton, RajshahiButton, SirajgongButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_rajshahi_district);
+
+        BograButton = (Button) findViewById ( R.id.BograDistrictId );
+        ChapaiButton = (Button) findViewById ( R.id.ChapaiDistrictId );
+        JoypurhatButton = (Button) findViewById ( R.id.JoypurhatDistrictId );
+        NaogaonButton = (Button) findViewById ( R.id.BograDistrictId );
+        NatoreButton = (Button) findViewById ( R.id.NatorDistrictId);
+        PabnaButton = (Button) findViewById ( R.id.PabnaDistrictId );
+        RajshahiButton = (Button) findViewById ( R.id.RajshahiDistrictId);
+        SirajgongButton= (Button) findViewById ( R.id.SirajgangDistrictId );
+
+        BograButton.setOnClickListener ( this );
+        ChapaiButton.setOnClickListener ( this );
+        JoypurhatButton.setOnClickListener ( this );
+        NaogaonButton.setOnClickListener ( this );
+        NatoreButton.setOnClickListener ( this );
+        PabnaButton.setOnClickListener ( this );
+        RajshahiButton.setOnClickListener ( this );
+        SirajgongButton.setOnClickListener ( this );
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId () == R.id.BograDistrictId)
+        {
+
+            Intent intent = new Intent ( RajshahiDistrict.this,BograRajshahi.class );
+            startActivity ( intent );
+        }
+
+//        else if (v.getId () == R.id.ChapaiDistrictId)
+//        {
+//
+//            Intent intent = new Intent ( RajshahiDistrict.this,RajshahiDistrict.class );
+//            startActivity ( intent );
+//        }
+
+
     }
 }
