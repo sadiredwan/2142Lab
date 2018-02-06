b807f952d3d897c17e97b33431c12447e78d311b
JOYRU <joycseru@gmail.com>
2018-02-06 23:54:48 +0600

    activity_chittagong_district layout add

--- app/src/main/java/com/nano/projectjonobak/divisions/ChittagongDistrict.java
+++ app/src/main/java/com/nano/projectjonobak/divisions/ChittagongDistrict.java
@@ -1,10 +1,19 @@
 package com.nano.projectjonobak.divisions;
 
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.nano.projectjonobak.R;
 /**
  * Created by User on 06-Feb-18.
  */
 
-public class ChittagongDistrict {
+public class ChittagongDistrict extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_rajshahi_district);
+    }
 
 }
