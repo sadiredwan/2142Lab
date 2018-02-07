c41c82b6ff0cb024ea0c035bf06548a500c85a1d
JOYRU <joycseru@gmail.com>
2018-02-07 23:11:30 +0600

    add joypurhat district and activity_joypurhat

--- app/src/main/java/com/nano/projectjonobak/divisions/districts/Chapainawabganj.java
+++ app/src/main/java/com/nano/projectjonobak/divisions/districts/Chapainawabganj.java
@@ -1,7 +1,9 @@
-package com.nano.projectjonobak;
+package com.nano.projectjonobak.divisions.districts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nano.projectjonobak.R;
 
 public class Chapainawabganj extends AppCompatActivity {
 
