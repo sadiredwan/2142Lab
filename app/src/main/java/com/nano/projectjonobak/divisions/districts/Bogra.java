6a34a47db84a815a88b9f16f10e422b238e4cf3d
sadiredwan <sadi.redwan@gmail.com>
2018-02-07 17:36:07 +0600

    PostActivity & corresponding layouts added.

--- app/src/main/java/com/nano/projectjonobak/divisions/districts/Bogra.java
+++ app/src/main/java/com/nano/projectjonobak/divisions/districts/Bogra.java
@@ -1,8 +1,12 @@
 package com.nano.projectjonobak.divisions.districts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.nano.projectjonobak.PostActivity;
 import com.nano.projectjonobak.R;
 
 public class Bogra extends AppCompatActivity {
@@ -11,5 +15,19 @@ public class Bogra extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_bogra_rajshahi );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_add) {
+            startActivity(new Intent(Bogra.this, PostActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
