7fe6085c6af71f090ed5c4563200bdf80d949f4e
JOYRU <joycseru@gmail.com>
2018-02-07 23:19:44 +0600

    add Naogaon district and activity_naogaon

--- app/src/main/java/com/nano/projectjonobak/Naogon.java
+++ app/src/main/java/com/nano/projectjonobak/Naogon.java
@@ -3,11 +3,11 @@ package com.nano.projectjonobak;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Joypurhat extends AppCompatActivity {
+public class Naogon extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_joypurhat);
+        setContentView(R.layout.activity_naogon);
     }
 }
