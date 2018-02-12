ee4bda94be784f26cf220f8b317bb0ce3e7e375f
JOYRU <joycseru@gmail.com>
2018-02-13 04:35:19 +0600

    adding post activity for rajshahi , sirajgang district in Rajshahi Division

--- app/src/main/java/com/nano/projectjonobak/divisions/districts/Sirajgang.java
+++ app/src/main/java/com/nano/projectjonobak/divisions/districts/Sirajgang.java
@@ -1,17 +1,28 @@
 package com.nano.projectjonobak.divisions.districts;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nano.projectjonobak.Post;
 import com.nano.projectjonobak.PostActivity;
 import com.nano.projectjonobak.R;
+import com.squareup.picasso.Picasso;
 
 public class Sirajgang extends AppCompatActivity {
 
-    @Override
+   /* @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sirajgang);
@@ -27,6 +38,76 @@ public class Sirajgang extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.action_add) {
             startActivity(new Intent(Sirajgang.this, PostActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
+
+    private RecyclerView postList;
+    private DatabaseReference database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_sirajgang );
+        database = FirebaseDatabase.getInstance().getReference().child("projectjonobak").child("posts").child("sirajgang");
+        postList = (RecyclerView) findViewById(R.id.post_list);
+        postList.setHasFixedSize(true);
+        postList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Post, Sirajgang.PostViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Post, Sirajgang.PostViewHolder>(
+                Post.class, R.layout.post_row, Sirajgang.PostViewHolder.class, database
+        ) {
+            @Override
+            protected void populateViewHolder(Sirajgang.PostViewHolder viewHolder, Post model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDescription(model.getDescription());
+                viewHolder.setImage(getApplicationContext(), model.getImage());
+            }
+        };
+        postList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder {
+
+        View view;
+        public PostViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+
+        public void setTitle(String title){
+            TextView post_title = (TextView) view.findViewById(R.id.post_title);
+            post_title.setText(title);
+        }
+
+        public void setDescription(String description){
+            TextView post_desc = (TextView) view.findViewById(R.id.post_description);
+            post_desc.setText(description);
+        }
+
+        public void setImage(Context ctx, String image){
+            ImageView post_image = (ImageView) view.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_add) {
+            Intent intent = new Intent(Sirajgang .this, PostActivity.class);
+            intent.putExtra("PARENT_ACTIVITY", "sirajgang");
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
